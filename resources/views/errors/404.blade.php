88c105bf79a37863ffe7b4ada501f0ff8cd56a17
Bojan Zarkovic <bojanzarkovic@gmail.com>
2021-03-01 22:18:15 +0100

    login page small fix

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,7 +6,7 @@
             <div class="col-sm pt-5">
                 <h4>404</h4>
                 <p>Page you are looking for is not found, here's a random cat gif to make you feel better:</p>
-                <div><img alt="funny cat gif" src="https://cataas.com/cat/gif"></div>
+                <div><img alt="funny cat gif" src="https://cataas.com/cat/gif?{{ rand() }}"></div>
             </div>
         </div>
     </div>
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -6,7 +6,7 @@
             <div class="col-sm pt-5">
                 <h4>500</h4>
                 <p>Unexpected error has occurred, sorry about that, here's a random cat gif to make you feel better:</p>
-                <div><img alt="funny cat gif" src="https://cataas.com/cat/gif"></div>
+                <div><img alt="funny cat gif" src="https://cataas.com/cat/gif?{{ rand() }}"></div>
             </div>
         </div>
     </div>
